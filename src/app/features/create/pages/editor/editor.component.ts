8dd38103508b036db863c19474afae40587b3d70
Aman Jha <amanj@pronnel.com>
2025-05-17 22:50:10 +0530

    added proper book and normal view with tools

--- src/app/features/create/pages/editor/editor.component.ts
+++ src/app/features/create/pages/editor/editor.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Editor, Toolbar } from 'ngx-editor';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  standalone: false,
+  standalone:false,
   styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent implements OnInit, OnDestroy {
-  editor!: Editor;
-  html: string = '';
-  viewMode: 'book' | 'normal' = 'book';
+  editors: Editor[] = [];
+  pages: string[] = [];
+  currentPageIndex = 0;
+  isAnimating = false;
 
   toolbar: Toolbar = [
     ['bold', 'italic', 'underline', 'strike'],
@@ -21,17 +22,64 @@ export class EditorComponent implements OnInit, OnDestroy {
     ['text_color', 'background_color'],
     ['align_left', 'align_center', 'align_right'],
     ['undo', 'redo'],
+    ['superscript', 'subscript', 'horizontal_rule'],
+    ['align_justify','format_clear','outdent','link','code']
   ];
 
-  ngOnInit(): void {
-    this.editor = new Editor();
+  viewMode: 'book' | 'normal' = 'book';
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.pages = ['', '']; // Start with two empty pages
+    // Create 2 editors for book view, 1 for normal view
+    this.editors = [new Editor(), new Editor()];
+  }
+
+  ngOnDestroy() {
+    this.editors.forEach(e => e.destroy());
   }
 
   toggleView() {
     this.viewMode = this.viewMode === 'book' ? 'normal' : 'book';
+    if (this.viewMode === 'normal' && this.editors.length > 1) {
+      // In normal view, only one editor needed
+      this.editors[1].destroy();
+      this.editors = [this.editors[0]];
+    } else if (this.viewMode === 'book' && this.editors.length === 1) {
+      // Back to book view, recreate second editor if missing
+      this.editors.push(new Editor());
+    }
   }
 
-  ngOnDestroy(): void {
-    this.editor.destroy();
+  nextPage() {
+    if (this.isAnimating) return;
+    this.isAnimating = true;
+
+    if (this.currentPageIndex + 2 >= this.pages.length) {
+      this.pages.push('', '');
+    }
+    this.currentPageIndex += 2;
+
+    // no need to recreate editors, just update view
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.cdr.detectChanges();
+    }, 400);
+  }
+
+  prevPage() {
+    if (this.isAnimating || this.currentPageIndex === 0) return;
+    this.isAnimating = true;
+    this.currentPageIndex -= 2;
+
+    setTimeout(() => {
+      this.isAnimating = false;
+      this.cdr.detectChanges();
+    }, 400);
+  }
+
+  getPageNumber(pageIndex: number): number {
+    return pageIndex + 1;
   }
 }
