07f35e353079ddbe4b050af49c7f4d72d41ace10
Aman Jha <amanj@pronnel.com>
2025-05-17 22:28:47 +0530

    added editor in create tab

--- src/app/features/create/create.module.ts
+++ src/app/features/create/create.module.ts
@@ -5,6 +5,8 @@ import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create.component';
 import { EditorComponent } from './pages/editor/editor.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NgxEditorModule } from 'ngx-editor';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
   ],
   imports: [
     CommonModule,
-    CreateRoutingModule
+    CreateRoutingModule,
+    NgxEditorModule,
+    FormsModule
   ]
 })
 export class CreateModule { }
--- src/app/features/create/pages/editor/editor.component.ts
+++ src/app/features/create/pages/editor/editor.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Editor, Toolbar } from 'ngx-editor';
 
 @Component({
   selector: 'app-editor',
-  standalone: false,
   templateUrl: './editor.component.html',
-  styleUrl: './editor.component.scss'
+  standalone: false,
+  styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent {
+export class EditorComponent implements OnInit, OnDestroy {
+  editor!: Editor;
+  html: string = '';
+  viewMode: 'book' | 'normal' = 'book';
 
+  toolbar: Toolbar = [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right'],
+    ['undo', 'redo'],
+  ];
+
+  ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  toggleView() {
+    this.viewMode = this.viewMode === 'book' ? 'normal' : 'book';
+  }
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 }
